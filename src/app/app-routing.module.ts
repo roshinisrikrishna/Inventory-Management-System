de13284c8a237d816040f48d7bd4885c9969bab2
Roshini <roshinisrikrishna@gmail.com>
2023-05-13 20:26:40 +0530

    Commit message describing the changes made

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddItemComponent } from './components/add-item/add-item.component';
+import { ViewItemsComponent } from './components/view-items/view-items.component';
+import { UpdateItemComponent } from './components/update-item/update-item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:ViewItemsComponent},
+  {path:'add',component:AddItemComponent},
+  {path:'update/:id',component:UpdateItemComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
